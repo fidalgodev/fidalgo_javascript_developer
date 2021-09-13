176dd747f2caabf820f86c8e7532ff58131b7056
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2021-09-13 18:44:00 +0100

    removed color from theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,13 +1,4 @@
 const theme = {
-  colors: {
-    black: '#1A1A1A',
-    // blackOpacity90: '#1A1A1AE6',
-    blackOpacity80: '#1A1A1ACC',
-    blackOpacity60: '#1A1A1A99',
-    white: '#FFFFFF',
-    whiteOpacity80: '#FFFFFFCC',
-    whiteOpacity60: '#FFFFFF99',
-  },
   mediaQueries: {
     smaller: 'only screen and (max-width: 31.25em)',
     small: 'only screen and (max-width: 37.5em)',
