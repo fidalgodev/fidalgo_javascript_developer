0c6b5d956e42963b316de9cdca0e54e7cf7651f8
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2021-09-13 19:49:22 +0100

    404 page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,10 +5,10 @@ import GlobalStyles from '../styles/global';
 import theme from '../styles/theme';
 import Navbar from './Navigation/Navbar';
 
-export const Layout = ({ children }) => {
+export const Layout = ({ children, noNav = false }) => {
   return (
     <ThemeProvider theme={theme}>
-      <Navbar />
+      {!noNav && <Navbar />}
       <main>{children}</main>
       <GlobalStyles />
     </ThemeProvider>
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 import { SubHeading, Heading } from '../styles/typography';
-import { ContainedWrapper } from '../styles/elements';
+
 import { NavLink } from '../components/Navigation/NavLink';
+import { Layout } from '../components/Layout';
+import { ContainedStyles } from '../styles/elements';
+
+const PageWrapper = styled.div`
+  ${ContainedStyles}
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+`;
 
 const NotFoundPage = () => {
   return (
-    <ContainedWrapper>
-      <SubHeading>Upps!</SubHeading>
-      <Heading>Sorry, this page doesn&#39;t exist.</Heading>
-      <NavLink to="/">Go Back home</NavLink>
-    </ContainedWrapper>
+    <Layout noNav>
+      <PageWrapper>
+        <SubHeading>Oops!</SubHeading>
+        <Heading>This page does not exist.</Heading>
+        <NavLink to="/">go back home</NavLink>
+      </PageWrapper>
+    </Layout>
   );
 };
 
