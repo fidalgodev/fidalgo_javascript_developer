4e0159d6131ea72b8be94e11f43fbc7015dd727c
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2021-09-13 18:49:53 +0100

    test

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -7,7 +7,7 @@ export const SubHeading = styled.h2`
   font-weight: 600;
   margin-bottom: 1rem;
   font-size: 1.6rem;
-  letter-spacing: 12px;
+  letter-spacing: 1px;
 `;
 
 export const Heading = styled.h1`
