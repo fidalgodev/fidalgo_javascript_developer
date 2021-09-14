ed0cd006f41777f0667a1c8efb515ca816a9c69d
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2021-09-14 12:17:09 +0100

    small fix

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -23,11 +23,11 @@ export const HomePage = () => {
         </p>
         <p>
           In October 2018 I decided it was time for a change, so I started to
-          wake up at 6am to study Javascript before going into work. I quickly
-          started to love Javascript and in January 2019 I decided to take the
-          risk and quit my job. I then studied Javascript until July 2019, where
-          I landed my first job at Mindera UK. I quickly improved my Javascript
-          skills here and transitioned to a Full Stack position.
+          study Javascript before going into work. I quickly started to love
+          Javascript and in January 2019 I decided to take the risk and quit my
+          job. I then studied Javascript until July 2019, where I landed my
+          first job at Mindera UK. I quickly improved my Javascript skills here
+          and transitioned to a Full Stack position.
         </p>
         <p>
           I always want to learn moreand do more. I&#39;m also a firm believer
