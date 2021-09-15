a1d3745952fb96edb3a7cc41b8af8b09f22e7d88
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2021-09-15 09:08:49 +0100

    small fix

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 
-import { NavLink } from './NavLink';
+// import { NavLink } from './NavLink';
 import { Header, HeaderContainer, Logo } from './style';
 
 const Navbar = () => {
@@ -21,9 +21,9 @@ const Navbar = () => {
           />
           fidalgo.dev
         </Logo>
-        <nav>
+        {/* <nav>
           <NavLink to="/">about</NavLink>
-        </nav>
+        </nav> */}
       </HeaderContainer>
     </Header>
   );
